ea5c14c0b81bdfa33fe679562d956a408ca6e651
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-08-07 14:17:54 -0700

    AirBnB console

--- console.py
+++ console.py
@@ -54,7 +54,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def default(self, arg):
-        """Default behavior for cmd module when input is invalid"""
+        """Default behaviour for cmd module when input is invalid"""
         dict_args = {
             "all": self.do_all,
             "show": self.do_show,
